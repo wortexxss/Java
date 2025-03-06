5330ccb1430a641e9f0b4a9cc975a98a30f5d7bc
wortexxss <ellvinabdurrahmanov@gmail.com>
2025-03-07 00:38:50 +0400

    Update JavaPractice.java

--- Dropbox/PC/Documents/NetBeansProjects/JavaPractice/src/main/java/java/practic/project/javapractice/JavaPractice.java
+++ Dropbox/PC/Documents/NetBeansProjects/JavaPractice/src/main/java/java/practic/project/javapractice/JavaPractice.java
@@ -13,6 +13,5 @@ public class JavaPractice {
     public static void main(String[] args) {
         System.out.println("Hello World!");
         
-        System.out.println("Suanda fuck mee Alizade ");
     }
 }
