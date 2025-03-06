587f24ce3143733e44f1d5c58cb9d6a4a5935a17
wortexxs <ellvinabdurrahmanov@gmail.com>
2025-03-06 23:28:08 +0400

    Bu bir test

---
+++ Dropbox/PC/Documents/NetBeansProjects/JavaPractice/src/main/java/java/practic/project/javapractice/JavaPractice.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package java.practic.project.javapractice;
+
+/**
+ *
+ * @author NITRO
+ */
+public class JavaPractice {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
